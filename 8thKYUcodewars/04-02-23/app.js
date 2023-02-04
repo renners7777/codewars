adc9a634db35fa97b6bdbb678cf8950d2466be1b
Chris Renshaw <104800006+renners7777@users.noreply.github.com>
2023-02-04 18:26:00 +0000

    Completed simpleMultiplication 8thKYU

---
+++ 8thKYUcodewars/04-02-23/app.js
@@ -0,0 +1,11 @@
+//# Simple multiplication
+
+function simpleMultiplication(number) {
+    // your code........
+    if (number % 2 === 0) {
+        return number * 8;
+
+    } else {
+        return number * 9;
+    }
+}
